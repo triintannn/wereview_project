93a0b5ab968d013fd9d773889c33d29bc91c04ad
Nugraha Adi Sulistyo <tyoku1792@gmail.com>
2020-01-15 10:17:08 +0700

    Add Fragment

--- app/src/main/java/com/example/wereview/MainActivity.java
+++ app/src/main/java/com/example/wereview/MainActivity.java
@@ -1,8 +1,19 @@
 package com.example.wereview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.net.wifi.aware.PeerHandle;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.wereview.ui._fragment.AddFragment;
+import com.example.wereview.ui._fragment.ChatFragment;
+import com.example.wereview.ui._fragment.HomeFragment;
+import com.example.wereview.ui._fragment.LoveFragment;
+import com.example.wereview.ui._fragment.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomnav= findViewById(R.id.bottom_navigation);
+        bottomnav.inflateMenu(R.menu.bottom_navigation);
+        bottomnav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_love:
+                            selectedFragment = new LoveFragment();
+                            break;
+                        case R.id.nav_add:
+                            selectedFragment = new AddFragment();
+                            break;
+                        case R.id.nav_chat:
+                            selectedFragment = new ChatFragment();
+                            break;
+                        case R.id.nav_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+
 }
